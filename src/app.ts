37b0f1e8025f1b0c3ddc12cf4964808cb2fc51bb
Rasel-231 <freelancingacc11@gmail.com>
2026-09-25 03:40:22 +0600

    update CORS configuration to enable local development

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ const app: Application = express();
 
 app.use(cors({
     origin: [
-        // "http://localhost:3000",
-        "https://rasel-portfolio-frontend.vercel.app"
+        "http://localhost:3000",
+        // "https://rasel-portfolio-frontend.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
